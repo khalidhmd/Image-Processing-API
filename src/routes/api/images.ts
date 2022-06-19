d3b39640dd13e12fdbbac4c8ce5f14f38bf3453e
khalidhmd <khalidhmd@outlook.com>
2022-06-19 18:36:54 +0200

    serve original images /api/images?file=

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const images = express.Router();
 
+// serve original images (no processing) form images folder
 images.get('/', (req, res) => {
-  res.send('images route');
+  const file = req.query.file;
+  res.sendFile(path.join(__dirname, '../../../images/') + file);
 });
 
 export default images;
